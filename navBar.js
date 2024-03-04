3a5c8ed99bbd1a09b39b239831f3b5e2a634e1f3
Vigneswaraa M S <106534879+msvicky29@users.noreply.github.com>
2024-03-04 16:59:53 +0530

    Add files via upload

---
+++ navBar.js
@@ -0,0 +1,45 @@
+let hambuger = document.querySelector(".HamBurger");
+let menu = document.querySelector(".Nav-ul");
+
+let a1Element = document.getElementById("1");
+let a2Element = document.getElementById("2");
+let a3Element = document.getElementById("3");
+let a4Element = document.getElementById("4");
+let a5Element = document.getElementById("5");
+
+
+hambuger.onclick = function () {
+  hambuger.classList.toggle("Active");
+  menu.classList.toggle("Active");
+}
+
+// window.onscroll = ()=>{
+//   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+// }
+
+
+a1Element.onclick = function () {
+  hambuger.classList.remove("Active");
+  menu.classList.remove("Active");
+
+}
+a2Element.onclick = function () {
+  hambuger.classList.remove("Active");
+  menu.classList.remove("Active");
+
+}
+a3Element.onclick = function () {
+  hambuger.classList.remove("Active");
+  menu.classList.remove("Active");
+
+}
+a4Element.onclick = function () {
+  hambuger.classList.remove("Active");
+  menu.classList.remove("Active");
+
+}
+a5Element.onclick = function () {
+  hambuger.classList.remove("Active");
+  menu.classList.remove("Active");
+
+}
